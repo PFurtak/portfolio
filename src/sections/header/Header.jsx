f087b3c0875cd37f1677c8a01d9c070c8412993e
Patrick Furtak <patrick@patrickfurtak.com>
2020-05-27 23:45:45 -0400

    Started laying out components, picked color theme, started on header

---
+++ src/sections/header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <span className='header-text-name'>Patrick Furtak</span>
+      <br />
+      <span className='header-text-jobtitle'>Software Engineer</span>
+    </div>
+  );
+};
+
+export default Header;
